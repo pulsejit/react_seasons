133c36b29608dd33c9d9879f60d18e379f3651fa
Subhajit Chatterjee <subhajitchatterjee425@gmail.com>
2020-05-23 07:58:57 +0530

    ternary op is used inside and outside of jsx to show some content from semantic ui

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -11,10 +11,19 @@ const getSeason = (lat,month)=>{
 
 let SeasonDisplay = (props)=>{
     let season = getSeason(props.lat, new Date().getMonth());
-
-    let mon = new Date().getMonth();
+    // if(season=='summer'){
+    //     return <div>let's hit the beach</div>
+    // }else{
+    //     return <div>Burr!! it's chilly</div>
+    // }
+    const text = season==='winter' ? 'Burr!!! it\'s chilly' : 'lets go to the beach';
+    const icon = season==='winter' ? 'snowflake' : 'sun'; 
     return(
-        <div>Season Display   {season}</div>
+        <div>
+            <i className={`${icon} loading icon`}></i>
+            <h1>{text}</h1>
+            <i className={`${icon} loading icon`}></i>
+        </div>
     );
 };
 
